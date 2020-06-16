7ef8b45cd97b5b6294945552b748f04aa9366397
Fumihiro Ito <fmhrit@gmail.com>
2020-06-16 23:10:48 +0900

    Include static content into the container of pkgsite

--- script/vendor-pkgsite.sh
+++ script/vendor-pkgsite.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
+set -e
 
 OWNER="golang"
 REPO="pkgsite"
 IMPORTPATH="golang.org/x/pkgsite"
 COMMIT="5ae7386a8ff7d9caec4c44f8ce68f402793f3954"
 
+PATCH_DIR=$(cd $(dirname $0)/${REPO}; pwd)
 THIRD_PARTY_DIR=$(cd $(dirname $0)/../third_party; pwd)
 TARGET_DIR="${THIRD_PARTY_DIR}/${REPO}"
 
@@ -34,3 +36,13 @@ go_vendor(name = "vendor")
 EOS
 
 bazel run //third_party/${REPO}:vendor
+
+for i in $(find $PATCH_DIR -type f)
+do
+  file=${i#"$PATCH_DIR/"}
+  if [ -f "$TARGET_DIR/$file" ]; then
+    cat "$PATCH_DIR/$file" >> "$TARGET_DIR/$file"
+  else
+    cp "$PATCH_DIR/$file" "$TARGET_DIR/$file"
+  fi
+done
