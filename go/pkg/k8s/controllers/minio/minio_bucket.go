bd28be1996ad469e727fc53e6d7d0ee1532d4b7e
Fumihiro Ito <fmhrit@gmail.com>
2021-05-02 13:22:19 +0900

    controllers: Update RBAC

--- go/pkg/k8s/controllers/minio/minio_bucket.go
+++ go/pkg/k8s/controllers/minio/minio_bucket.go
@@ -41,7 +41,7 @@ import (
 // +kubebuilder:rbac:groups=minio.f110.dev,resources=miniobuckets,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=minio.f110.dev,resources=miniobuckets/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=miniocontroller.min.io,resources=minioinstances,verbs=get;list;watch
-// +kubebuilder:rbac:groups=*,resources=pods;secrets;services,verbs=get
+// +kubebuilder:rbac:groups=*,resources=pods;secrets;services,verbs=get;list;watch
 // +kubebuilder:rbac:groups=*,resources=pods/portforward,verbs=get;list;create
 
 const (
