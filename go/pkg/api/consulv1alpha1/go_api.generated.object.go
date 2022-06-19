b0969e2520f7d549e22c7b3afb903874b9ca8a4c
Fumihiro Ito <fmhrit@gmail.com>
2022-06-19 12:11:36 +0900

    api: Generate

--- go/pkg/api/consulv1alpha1/go_api.generated.object.go
+++ go/pkg/api/consulv1alpha1/go_api.generated.object.go
@@ -4,7 +4,26 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
+
+const GroupName = "consul.f110.dev"
+
+var (
+	GroupVersion       = metav1.GroupVersion{Group: GroupName, Version: "v1alpha1"}
+	SchemeBuilder      = runtime.NewSchemeBuilder(addKnownTypes)
+	AddToScheme        = SchemeBuilder.AddToScheme
+	SchemaGroupVersion = schema.GroupVersion{Group: "consul.f110.dev", Version: "v1alpha1"}
+)
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemaGroupVersion,
+		&ConsulBackup{},
+		&ConsulBackupList{},
+	)
+	metav1.AddToGroupVersion(scheme, SchemaGroupVersion)
+	return nil
+}
 
 type ConsulBackup struct {
 	metav1.TypeMeta   `json:",inline"`
--- go/pkg/api/grafanav1alpha1/go_api.generated.object.go
+++ go/pkg/api/grafanav1alpha1/go_api.generated.object.go
@@ -4,7 +4,28 @@ import (
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/runtime/schema"
 )
+
+const GroupName = "grafana.f110.dev"
+
+var (
+	GroupVersion       = metav1.GroupVersion{Group: GroupName, Version: "v1alpha1"}
+	SchemeBuilder      = runtime.NewSchemeBuilder(addKnownTypes)
+	AddToScheme        = SchemeBuilder.AddToScheme
+	SchemaGroupVersion = schema.GroupVersion{Group: "grafana.f110.dev", Version: "v1alpha1"}
+)
+
+func addKnownTypes(scheme *runtime.Scheme) error {
+	scheme.AddKnownTypes(SchemaGroupVersion,
+		&Grafana{},
+		&GrafanaList{},
+		&GrafanaUser{},
+		&GrafanaUserList{},
+	)
+	metav1.AddToGroupVersion(scheme, SchemaGroupVersion)
+	return nil
+}
 
 type Grafana struct {
 	metav1.TypeMeta   `json:",inline"`
