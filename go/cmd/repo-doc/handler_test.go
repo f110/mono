9e082d34d2d159cd61b6fae147a0c8290b087a1f
Fumihiro Ito <fmhrit@gmail.com>
2022-07-02 12:53:25 +0900

    repo-doc: Add test cases

--- go/cmd/repo-doc/handler_test.go
+++ go/cmd/repo-doc/handler_test.go
@@ -9,10 +9,40 @@ import (
 )
 
 func TestHandler(t *testing.T) {
+	cases := []struct {
+		URL      string
+		Repo     string
+		Ref      string
+		FilePath string
+	}{
+		{
+			URL:      "http://example.com/test1/master/-/docs/README.md",
+			Repo:     "test1",
+			Ref:      "master",
+			FilePath: "docs/README.md",
+		},
+		{
+			URL:      "http://example.com/test1/feature/update-doc/-/docs/README.md",
+			Repo:     "test1",
+			Ref:      "feature/update-doc",
+			FilePath: "docs/README.md",
+		},
+		{
+			URL:      "http://example.com/test1/8e6e2933140691846d824231bde4af011200cf5a/-/docs/README.md",
+			Repo:     "test1",
+			Ref:      "8e6e2933140691846d824231bde4af011200cf5a",
+			FilePath: "docs/README.md",
+		},
+	}
+
 	h := newHttpHandler(nil)
-	req := httptest.NewRequest(http.MethodGet, "http://example.com/test1/feature/update-doc/-/docs/README.md", nil)
-	repo, ref, filepath := h.parsePath(req)
-	assert.Equal(t, "test1", repo)
-	assert.Equal(t, "feature/update-doc", ref)
-	assert.Equal(t, "docs/README.md", filepath)
+	for _, tc := range cases {
+		t.Run(tc.URL, func(t *testing.T) {
+			req := httptest.NewRequest(http.MethodGet, tc.URL, nil)
+			repo, ref, filepath := h.parsePath(req)
+			assert.Equal(t, tc.Repo, repo)
+			assert.Equal(t, tc.Ref, ref)
+			assert.Equal(t, tc.FilePath, filepath)
+		})
+	}
 }
