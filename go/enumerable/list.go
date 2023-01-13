4c6ad0ef613146ec4cc435e23c9076278cf09219
Fumihiro Ito <fmhrit@gmail.com>
2023-01-14 03:19:12 +0900

    enumerable: Add Delete function

--- go/enumerable/list.go
+++ go/enumerable/list.go
@@ -9,3 +9,14 @@ func IsInclude[T comparable](ary []T, val T) bool {
 
 	return false
 }
+
+func Delete[T comparable](ary []T, val T) []T {
+	n := make([]T, 0, len(ary))
+	for _, v := range ary {
+		if v == val {
+			continue
+		}
+		n = append(n, v)
+	}
+	return n
+}
