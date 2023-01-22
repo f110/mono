5dd029aaff4dbc8504c6489a2777f8e9dec96b9a
Fumihiro Ito <fmhrit@gmail.com>
2023-01-22 12:49:10 +0900

    logger: Add Verbose function

--- go/logger/xerrors.go
+++ go/logger/xerrors.go
@@ -1,6 +1,8 @@
 package logger
 
 import (
+	"errors"
+
 	"go.f110.dev/xerrors"
 	"go.uber.org/zap"
 )
@@ -10,6 +12,20 @@ func Error(err error) zap.Field {
 		return zap.Skip()
 	}
 	return zap.String("error", err.Error())
+}
+
+func Verbose(err error) zap.Field {
+	for {
+		v, ok := err.(interface{ Verbose() string })
+		if ok {
+			return zap.String("verbose", v.Verbose())
+		}
+		err = errors.Unwrap(err)
+		if err == nil {
+			break
+		}
+	}
+	return zap.Skip()
 }
 
 func StackTrace(err error) zap.Field {
